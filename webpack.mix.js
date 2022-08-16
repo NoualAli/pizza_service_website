c83dc5d082d9298819a8198d9ed15529ee5c6b1c
Ali Noual <noualdev@gmail.com>
2022-08-16 19:22:35 +0100

    installation de vue js*

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,14 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-mix.js('resources/js/website/app.js', 'public/js/website');
+mix.disableNotifications()
+// mix.js('resources/js/website/app.js', 'public/js/website');
 mix.sass('resources/sass/website/app.sass', 'public/css/website').options({
     autoprefixer: { remove: false },
     stats: { children: true },
 }).version();
+
+mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/home.js', 'public/js')
+mix.js('resources/js/restaurants.js', 'public/js')
+    .vue()
+
