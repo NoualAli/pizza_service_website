4e3a9d80dca98e7908d15630d49772731ee5d686
Ali Noual <noualdev@gmail.com>
2022-08-16 19:22:56 +0100

    Ajout d'une route pour la liste des restaurants

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Resources\RestaurantResource;
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+Route::get('restaurants-list', fn () => RestaurantResource::collection(Restaurant::all()));
