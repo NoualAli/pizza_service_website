f31ba3025ae7ab177cf12b9635a6845bd2ef88ab
Ali Noual <noualdev@gmail.com>
2022-08-17 12:11:19 +0100

    first commit

---
+++ resources/views/website/includes/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="bg-light text-center p-5">
+    <p>
+        Pizza Service by
+        <a href="gogofood.fi" class="text-decoration-none" target="_blank">
+            GoGo Food Finland oy
+        </a>
+        | &copy; 2022 All rights reserved!
+    </p>
+</footer>
--- resources/views/website/includes/preloader.blade.php
+++ resources/views/website/includes/preloader.blade.php
@@ -1,1 +1,11 @@
-<div id="preloader"></div>
+<div id="preloader" class="is-active"></div>
+@once
+    @push('scripts')
+        <script type="text/javascript">
+            const preloader = document.querySelector('#preloader')
+            window.addEventListener('DOMContentLoaded', () => {
+                preloader.classList.remove('is-active')
+            })
+        </script>
+    @endpush
+@endonce
