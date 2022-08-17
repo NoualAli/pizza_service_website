0ed7d1b4af8d8959072cf92945cd355ead929ff9
Ali Noual <noualdev@gmail.com>
2022-08-17 12:12:50 +0100

    correction des liens

--- resources/js/components/MainNavbar.vue
+++ resources/js/components/MainNavbar.vue
@@ -7,14 +7,14 @@
             </div>
             <div class="collapse navbar-collapse row" id="navbarSupportedContent">
                 <div class="col-lg-6">
-                    <a class="navbar-brand mt-2 mt-lg-0" href="/">
+                    <a class="navbar-brand mt-2 mt-lg-0" href="home">
                         <img src="assets/brand.png" height="45" alt="{{ env('APP_NAME') }} Logo" loading="lazy" />
                     </a>
                 </div>
                 <div class="col-lg-6">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="/">Home</a>
+                            <a class="nav-link" href="home">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="restaurants">Restaurants</a>
