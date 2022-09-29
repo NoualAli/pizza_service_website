0317596cd2751f926172f191cb3a559d566b0c68
Ali Noual <noualdev@gmail.com>
2022-09-29 14:46:27 +0100

    Fix bug

--- resources/js/website/Components/restaurant/RestaurantsList.vue
+++ resources/js/website/Components/restaurant/RestaurantsList.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <div class="container my-5">
-            <order-configuration @orderTypeChanged="this.fetchRestaurants" @clientPlaceChanged="this.fetchRestaurants"
+            <order-configuration @orderTypeChanged="this.filterByOrderType" @clientPlaceChanged="this.fetchRestaurants"
                 :showAddressInput="false">
             </order-configuration>
-            <CategoriesList @categoryChanged="this.filterRestaurants" @categoryCleared="this.fetchRestaurants">
+            <CategoriesList @categoryChanged="this.filterByCatgegory" @categoryCleared="this.fetchRestaurants">
             </CategoriesList>
         </div>
         <Transition name="fade">
@@ -38,6 +38,7 @@ export default {
             restaurants: {},
             categories: {},
             currentCategory: null,
+            urlParams: '',
         }
     },
 
@@ -53,11 +54,37 @@ export default {
             })
         },
 
-        async filterRestaurants(data) {
-            await axios.get(url('api/restaurants?category_id=' + data.id)).then((result) => {
-                this.currentCategory = data.id
+        async filterByCatgegory(data) {
+            await axios.get(url('api/restaurants?category_id=' + data.category.id)).then((result) => {
+                this.currentCategory = data.category.id
                 this.restaurants = result.data.restaurants
             })
+        },
+
+        async filterByOrderType(data) {
+            if (data.order_type) {
+                await axios.get(url('api/restaurants?order_type=' + data.order_type)).then((result) => {
+                    this.restaurants = result.data.restaurants
+                })
+            } else {
+                this.fetchRestaurants()
+            }
+        },
+
+        buildUrlParams(key, value) {
+            this.urlParams = this.urlParams?.split('&').map(param => {
+                return param.substring(0, 1) == '?' ? param.substring(1) : param
+            })
+            console.log(this.urlParams);
+
+            let separator = this.urlParams?.length > 1 ? '&' : '?'
+            let param = `${separator}${key}=${value}`
+            this.urlParams.push(param)
+            // console.log(this.urlParams);
+            // this.urlParams = [...new Set(this.urlParams)]
+
+            // this.urlParams = this.urlParams.join('')
+            // console.log(this.urlParams);
         }
     }
 }
